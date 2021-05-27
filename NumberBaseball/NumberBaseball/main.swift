c0df6c68f903509a71cce2a0ff7e1b594445382e
filmcanister <filmcanister97@gmail.com>
2021-05-27 09:23:02 +0900

    추가됨: 메소드 모두 구현 완료

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -4,66 +4,133 @@
 //  Copyright © yagom academy. All rights reserved.
 // 
 
-import Foundation
 
-//컴퓨터가 만드는 숫자
-func numGenerator() -> [Int] {
-    var randomNumbers = [Int]()
-    while randomNumbers.count < 3 {
-        let randomNumber = Int.random(in: 1...9)
-        if !randomNumbers.contains(randomNumber) {
-            randomNumbers.append(randomNumber)
-        }
+//게임시작시 사용자 입력 받기 메서드
+func chooseOption() -> Int {
+    print(
+        """
+        1. 게임시작
+        2. 게임종료
+        """)
+    print("원하는 기능을 선택해주세요 : ", terminator: "")
+    
+    //Unwrapping 및 숫자 아닐때 다시 입력받기
+    guard let input = readLine(), let numInput = Int(input) else {
+        print("입력이 잘못되었습니다")
+        return chooseOption()
     }
+    
+    //정상입력 return, 잘못된입력 다시 입력받기
+    if numInput == 1 || numInput == 2 {
+        return numInput
+    } else {
+        print("입력이 잘못되었습니다")
+        return chooseOption()
+    }
+}
+
+
+//게임 숫자 입력 메서드
+func inputUserNumbers() -> [Int] {
+    print(
+        """
+        숫자 3개를 띄어쓰기로 구분하여 입력해주세요.
+        중복 숫자는 허용하지 않습니다.
+        """)
+    print("입력 : ", terminator: "")
+    
+    //Unwrapping input
+    guard let input = readLine() else {
+        print("입력이 잘못되었습니다")
+        return inputUserNumbers()
+    }
+    
+    //입력값 정리 후 조건 확인
+    //띄어쓰기 기준으로 입력값 분리 후 Int로 형변환
+    var revisedNums = input.split(separator: " ").compactMap{Int($0)}
+    // 1...9 외의 값이 남아있으면 전부 제거됨
+    revisedNums.removeAll { value in
+        return (value > 9) || (value < 1)
+    }
+    if Set(revisedNums).count != 3 {
+        print("입력이 잘못되었습니다")
+        return inputUserNumbers()
+    }
+    
+    return revisedNums
+}
+
+
+//랜덤 숫자 생성 메서드
+func generateNumbers() -> [Int] {
+    var randomNumbers = [Int]()
+    
+    //중복제거 후 count 3 될때까지 반복
+    while randomNumbers.count < 3 {
+        let value = Int.random(in: 1...9)
+        randomNumbers.append(value)
+        //새로추가된 내용이 중복이라면 바로 다시 지워질 것
+        randomNumbers = Array(Set(randomNumbers))
+    }
+    
     return randomNumbers
 }
 
-var answer = numGenerator()
-var query = numGenerator()
-var leftCount = 9
 
-
-func answerJudge(attempt: [Int]) -> String {
-    var strike = 0
+//정답 비교 메서드
+func answerJudge(answer: [Int], query: [Int]) -> Bool {
     var ball = 0
-//    let answerSet = Set(answer)
-//    let querySet = Set(attempt)
-//    let ballNum = answerSet.intersection(querySet)
-//    ball = ballNum.count
+    var strike = 0
     
-    for index in 0...2 {
-        if answer[index] == attempt[index] {
-            strike += 1
-        } else if answer.contains(attempt[index]) {
-            ball += 1
-        }
+    //ball과 strike 개수 카운트
+    for (index, value) in answer.enumerated() {
+        strike += (value == query[index]) ? 1 : 0
+        ball += (query.contains(value)) ? 1 : 0
     }
+    ball -= strike
+    print("\(strike) 스트라이크, \(ball) 볼")
     
+    
+    //strike가 3이면 사용자 승리 및 게임 다시시작
+    //true 반환 : 사용자승리, false 반환 : default
     if strike == 3 {
-        return "사용자 승리...!"
+        print("사용자 승리!")
+        return true
+    } else {
+        return false
+    }
+}
+
+
+//게임 시작 메서드
+func startGame() -> Bool {
+    
+    //2를 선택할 경우 게임종료
+    if chooseOption() == 2 {
+        return false
     }
     
-    return "\(strike) 스트라이크, \(ball) 볼"
+    //정답 및 count 생성
+    let answer = generateNumbers()
+    var count = 9
     
+    //게임 9회 반복
+    while count > 0 {
+        count -= 1
+        if answerJudge(answer: answer, query: inputUserNumbers()) { return true }
+        print("남은 기회 : \(count)")
+    }
+    
+    //컴퓨터 승리 판별
+    print("컴퓨터 승리...!")
+    return true
 }
 
 
 
-func gameStart() {
-    while leftCount > 0 {
-        print(answer)
-        print(query)
-        leftCount -= 1
-        print("임의의 수 : \(query[0]) \(query[1]) \(query[2])")
-        
-        if leftCount == 0 && answer != query {
-            print("컴퓨터 승리...!")
-        }
-        
-        print(answerJudge(attempt: query))
-        print("남은 기회 : \(leftCount)")
-        query = numGenerator()
-    }
-}
+//게임시작
+var isGoingOn = true
 
-gameStart()
+while isGoingOn {
+    isGoingOn = startGame()
+}
